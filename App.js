7684a0c11edc1458396bf31a00e29d744c39982b
Badalchaudhary123 <114157836+Badalchaudhary123@users.noreply.github.com>
2023-01-07 12:17:20 +0530

    upload a file

---
+++ App.js
@@ -0,0 +1,15 @@
+import InputQuery from "./Components/InputQuery";
+import Product from "./Components/Project";
+import Subheading from "./Components/SubHeading";
+import Sumbitbutton from "./Components/SubmitButton";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
